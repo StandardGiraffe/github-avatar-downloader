458525360be8b91aa5be7a3fe514c9566984365f
StandardGiraffe <dannyfekete@gmail.com>
2018-09-03 22:33:44 +0000

    Leveraged my GitHub API token through secrets.js, which should not be added due to its inclusion in .gitignore.

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,20 @@
 const request = require("request");
+const secrets = require("./secrets");
 
 console.log("Welcome to the GitHub Avatar Downloader!");
 
 function getRepoContributors(repoOwner, repoName, callback) {
-  // ...
+  const options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+    headers: {
+      "User-Agent": "request",
+      "Authorization": "secrets.GITHUB_TOKEN"
+    }
+  };
+
+  request(url, function (err, result, body) {
+    callback(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function (err, result) {
