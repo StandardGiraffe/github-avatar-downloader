5b8fcd0d16722c79a3afb0764f7c8915b84909b7
StandardGiraffe <dannyfekete@gmail.com>
2018-09-04 01:25:14 +0000

    Commented code for clarity.

--- download_avatars.js
+++ download_avatars.js
@@ -2,13 +2,15 @@ const request = require("request");
 const fs = require("fs");
 const secrets = require("./secrets");
 
-console.log("Welcome to the GitHub Avatar Downloader!");
+console.log("Welcome to the GitHub Avatar Downloader!\n");
 
 function getRepoContributors(repoOwner, repoName, callback) {
   if (!repoOwner || !repoName) {
-    return console.log(`Expected arguments: <Name of Repository Owner> <Name of Repository>`);
+    return console.log(`\nExpected arguments: <Name of Repository Owner> <Name of Repository>\nPlease try again.\n`);
   }
 
+  // Inserts user input into the API.
+  // For reliable use, add your own token to ./secrets.js {GITHUB_TOKEN: "token <string>"}
   const options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
@@ -23,35 +25,32 @@ function getRepoContributors(repoOwner, repoName, callback) {
   });
 }
 
+// Iterates through the downloaded JSON and writes avatars to ./avatars/ as <username>.jpg
+// ### KNOWN BUG: Will throw an error if ./avatars/ does not exist.
+// ### KNOWN BUG: Does not distinguish file-type of target avatar.  As a result, if the written file is not formatted as a jpeg, it will not open.
 function processResponse(err, result) {
   console.log("Errors:", err);
   console.log("Results:\n\n")
   for (const i of result) {
-    // console.log("username: " + i.login + "; avatar URL: " + i.avatar_url);
     downloadImageByURL(i.avatar_url, "./avatars/" + i.login + ".jpg");
   }
 
 }
 
+// Takes URL provided by parsed JSON via API and pipes it to the write stream.
 const downloadImageByURL = function (url, destinationPath) {
   request
     .get(url)
-    // .on("error", function (err) {
-    //   console.log(err);
-    //   throw err;
-    // })
     .on("response", function (response) {
       console.log (`Downloading file: ${url} ...`);
     })
-    .pipe(fs.createWriteStream(destinationPath)) //&& console.log(`Saved to ${destinationPath}`));
+    .pipe(fs.createWriteStream(destinationPath))
 
 }
 
+// Takes user input: <Repository Owner> <Repository Name>
 const owner = process.argv[2];
 const name = process.argv[3];
 
+// Executes code from user input.
 getRepoContributors(owner, name, processResponse);
-
-
-
-// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
